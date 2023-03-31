85123609a0d9f5f3e0ec6d5b84835afd8458b274
Irati <iratigarzon@gmail.com>
2023-03-31 09:15:39 +0200

    Creation of the classes Administrator, Champ and Game

--- FinalChallenge-LoL/src/model/Game.java
+++ FinalChallenge-LoL/src/model/Game.java
@@ -1,5 +1,27 @@
 package model;
 
-public class Game {
+import java.time.LocalDate;
 
+public class Game {
+ private String id;
+ private LocalDate dateGame;
+ private Float duration;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public LocalDate getDateGame() {
+	return dateGame;
+}
+public void setDateGame(LocalDate dateGame) {
+	this.dateGame = dateGame;
+}
+public Float getDuration() {
+	return duration;
+}
+public void setDuration(Float duration) {
+	this.duration = duration;
+}
 }
