987693a7a93466d48289d0c19eca8066d72ebacf
Alex Salinero Negro <alexsalineronegro@gmail.com>
2023-04-18 08:26:28 +0200

    First instance of login window, 'images' path added for icon and additional images for project

--- FinalChallenge-LoL/src/gui/login.java
+++ FinalChallenge-LoL/src/gui/login.java
@@ -5,10 +5,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textUser;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -30,12 +42,66 @@ public class login extends JFrame {
 	 * Create the frame.
 	 */
 	public login() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\alexs\\OneDrive\\Documents\\FP\\Reto\\Workspace\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\icon\\LoL_icon.svg.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(36, 57, 117));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUserName = new JLabel("Username:");
+		lblUserName.setForeground(new Color(255, 255, 255));
+		lblUserName.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUserName.setBounds(60, 78, 81, 14);
+		contentPane.add(lblUserName);
+		
+		textUser = new JTextField();
+		textUser.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		textUser.setBounds(155, 77, 150, 20);
+		contentPane.add(textUser);
+		textUser.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setForeground(new Color(255, 255, 255));
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPassword.setBounds(60, 121, 81, 14);
+		contentPane.add(lblPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(155, 120, 150, 20);
+		contentPane.add(passwordField);
+		
+		JLabel lblNoAccount = new JLabel("No account?");
+		lblNoAccount.setForeground(new Color(255, 255, 255));
+		lblNoAccount.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNoAccount.setBounds(95, 184, 91, 14);
+		contentPane.add(lblNoAccount);
+		
+		JButton btnSignIn = new JButton("Sign in");
+		btnSignIn.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnSignIn.setBounds(213, 182, 89, 23);
+		contentPane.add(btnSignIn);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnCancel.setBounds(335, 227, 89, 23);
+		contentPane.add(btnCancel);
+		
+		JButton btnLogIn = new JButton("Log in");
+		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLogIn.setBounds(233, 229, 89, 23);
+		contentPane.add(btnLogIn);
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setIcon(new ImageIcon("C:\\Users\\alexs\\OneDrive\\Documents\\FP\\Reto\\Workspace\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\League-of-Legends-Logo-resize.png"));
+		lblLogo.setBounds(165, 11, 120, 55);
+		contentPane.add(lblLogo);
 	}
-
 }
