9a3a7c57dd1ed79c7618a7caa67f57697f716948
Irati <iratigarzon@gmail.com>
2023-04-19 11:41:27 +0200

    Changes in the login design

--- FinalChallenge-LoL/src/gui/Login.java
+++ FinalChallenge-LoL/src/gui/Login.java
@@ -11,103 +11,120 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 
-
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
 import java.awt.Color;
 
-public class Login extends JDialog {
-
-	private JPanel contentPane;
-	private JTextField textUser;
+public class Login extends JDialog implements ActionListener {
+	private final JPanel contentPanel = new JPanel();
 	private JPasswordField passwordField;
+	private JTextField Username_1;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		try {
+			Login dialog = new Login();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the dialog.
 	 */
 	public Login() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\alexs\\OneDrive\\Documents\\FP\\Reto\\Workspace\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\icon\\LoL_icon.svg.png"));
-		setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 330);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(36, 57, 117));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Aussar\\eclipse-workspace1\\pruebaVentanas\\Resources\\LoL_icon.svg (1) (1).png"));
+		setResizable(false);
+		setBounds(100, 100, 708, 431);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(248, 248, 255));
+		panel.setBounds(0, 0, 692, 450);
+		contentPanel.add(panel);
+		panel.setLayout(null);
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		JLabel Username = new JLabel("USERNAME");
 		
-		JLabel lblUserName = new JLabel("Username:");
-		lblUserName.setForeground(new Color(255, 255, 255));
-		lblUserName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblUserName.setBounds(60, 78, 81, 14);
-		contentPane.add(lblUserName);
 		
-		textUser = new JTextField();
-		textUser.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textUser.setBounds(155, 77, 150, 20);
-		contentPane.add(textUser);
-		textUser.setColumns(10);
+		JLabel SALIR = new JLabel("");
 		
-		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setForeground(new Color(255, 255, 255));
-		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblPassword.setBounds(60, 121, 81, 14);
-		contentPane.add(lblPassword);
+		JLabel SignIn = new JLabel("SIGN IN");
+		
+		SignIn.setBounds(174, 293, 85, 28);
+		panel.add(SignIn);
+		SignIn.setFont(new Font("Bahnschrift", Font.BOLD, 13));
+		SignIn.setForeground(Color.BLACK);
+		SALIR.setBounds(126, 337, 55, 50);
+		panel.add(SALIR);
+		SALIR.setIcon(new ImageIcon("C:\\Users\\irati\\Desktop\\RETOFINAL\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\salir.png"));
+		Username.setForeground(new Color(0, 0, 0));
+		Username.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Username.setBounds(43, 99, 178, 41);
+		panel.add(Username);
+		
+		JPanel PanelTransparente = new JPanel();
+		PanelTransparente.setBorder(null);
+		PanelTransparente.setBackground(new Color(100, 149, 237, 200));
+		PanelTransparente.setBounds(0, 0, 316, 398);
+		panel.add(PanelTransparente);
+		PanelTransparente.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.addMouseListener(new MouseAdapter() {
+			
+		});
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\irati\\Desktop\\RETOFINAL\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\lolLogo (2).png"));
+		lblNewLabel_1.setBounds(126, 25, 64, 64);
+		PanelTransparente.add(lblNewLabel_1);
+		
+		JLabel lblPassword = new JLabel("PASSWORD");
+		lblPassword.setBounds(43, 191, 178, 41);
+		PanelTransparente.add(lblPassword);
+		lblPassword.setForeground(Color.BLACK);
+		lblPassword.setFont(new Font("Bahnschrift", Font.BOLD, 17));
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(155, 120, 150, 20);
-		contentPane.add(passwordField);
+		passwordField.setBounds(43, 243, 203, 20);
+		PanelTransparente.add(passwordField);
+		passwordField.addActionListener(this);
 		
-		JLabel lblNoAccount = new JLabel("No account?");
-		lblNoAccount.setForeground(new Color(255, 255, 255));
-		lblNoAccount.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNoAccount.setBounds(98, 171, 91, 14);
-		contentPane.add(lblNoAccount);
+		Username_1 = new JTextField();
+		Username_1.setBounds(43, 144, 203, 20);
+		PanelTransparente.add(Username_1);
+		Username_1.setColumns(10);
 		
-		JButton btnSignIn = new JButton("Sign in");
-		btnSignIn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnSignIn.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnSignIn.setBounds(216, 169, 89, 23);
-		contentPane.add(btnSignIn);
+		JLabel lblNewLabel_3 = new JLabel("No account?");
+		lblNewLabel_3.setForeground(Color.BLACK);
+		lblNewLabel_3.setFont(new Font("Bahnschrift", Font.BOLD, 13));
+		lblNewLabel_3.setBounds(54, 298, 92, 17);
+		PanelTransparente.add(lblNewLabel_3);
+		Username_1.addActionListener(this);
 		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnCancel.setBounds(335, 255, 89, 23);
-		contentPane.add(btnCancel);
 		
-		JButton btnLogIn = new JButton("Log in");
-		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnLogIn.setBounds(233, 257, 89, 23);
-		contentPane.add(btnLogIn);
 		
-		JLabel lblLogo = new JLabel("");
-		lblLogo.setIcon(new ImageIcon("C:\\Users\\alexs\\OneDrive\\Documents\\FP\\Reto\\Workspace\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\League-of-Legends-Logo-resize.png"));
-		lblLogo.setBounds(165, 11, 120, 55);
-		contentPane.add(lblLogo);
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\irati\\Desktop\\RETOFINAL\\FinalChallenge-LoL\\FinalChallenge-LoL\\images\\fondo.jpg"));
+		lblNewLabel.setBounds(-57, -38, 924, 477);
+		panel.add(lblNewLabel);
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 	}
 }
