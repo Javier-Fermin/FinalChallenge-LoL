0a69a5138ce50d35f4671e7febad28479201e146
Irati <iratigarzon@gmail.com>
2023-04-19 12:50:14 +0200

    Creation of the view: Main Window

---
+++ FinalChallenge-LoL/src/gui/MainWindow.java
@@ -0,0 +1,161 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class MainWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField Nickname_TextField;
+	private JTextField Email_textField;
+	private JTextField textField_1;
+	private JTextField Phone_textField;
+	private JTextField Password_textField;
+	private JTextField Nacionality_textField;
+	private JTextField StartDate_textField;
+	private JTextField Additions_textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1210, 703);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 1196, 656);
+		contentPane.add(tabbedPane);
+		
+		JPanel profile = new JPanel();
+		tabbedPane.addTab("PROFILE\r\n", null, profile, null);
+		profile.setLayout(null);
+		
+		JLabel Nickname = new JLabel("NICKNAME");
+		Nickname.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Nickname.setBounds(64, 96, 108, 28);
+		profile.add(Nickname);
+		
+		Nickname_TextField = new JTextField();
+		Nickname_TextField.setBounds(64, 134, 191, 19);
+		profile.add(Nickname_TextField);
+		Nickname_TextField.setColumns(10);
+		
+		JLabel Email = new JLabel("EMAIL");
+		Email.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Email.setBounds(64, 244, 108, 28);
+		profile.add(Email);
+		
+		Email_textField = new JTextField();
+		Email_textField.setColumns(10);
+		Email_textField.setBounds(64, 271, 191, 19);
+		profile.add(Email_textField);
+		
+		JLabel Birthdate_textField = new JLabel("");
+		Birthdate_textField.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Birthdate_textField.setBounds(64, 342, 108, 28);
+		profile.add(Birthdate_textField);
+		
+		JLabel Birthdate = new JLabel("BIRTHDATE");
+		Birthdate.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Birthdate.setBounds(64, 314, 108, 28);
+		profile.add(Birthdate);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(64, 352, 191, 19);
+		profile.add(textField_1);
+		
+		JLabel lblPhone = new JLabel("PHONE");
+		lblPhone.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		lblPhone.setBounds(64, 399, 108, 28);
+		profile.add(lblPhone);
+		
+		Phone_textField = new JTextField();
+		Phone_textField.setColumns(10);
+		Phone_textField.setBounds(64, 437, 191, 19);
+		profile.add(Phone_textField);
+		
+		JLabel Password = new JLabel("PASSWORD");
+		Password.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Password.setBounds(64, 174, 108, 28);
+		profile.add(Password);
+		
+		Password_textField = new JTextField();
+		Password_textField.setColumns(10);
+		Password_textField.setBounds(64, 212, 191, 19);
+		profile.add(Password_textField);
+		
+		JLabel Nacionality = new JLabel("NACIONALITY");
+		Nacionality.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Nacionality.setBounds(64, 490, 156, 28);
+		profile.add(Nacionality);
+		
+		Nacionality_textField = new JTextField();
+		Nacionality_textField.setColumns(10);
+		Nacionality_textField.setBounds(64, 528, 191, 19);
+		profile.add(Nacionality_textField);
+		
+		JLabel StartDate = new JLabel("START DATE");
+		StartDate.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		StartDate.setBounds(494, 174, 108, 28);
+		profile.add(StartDate);
+		
+		StartDate_textField = new JTextField();
+		StartDate_textField.setColumns(10);
+		StartDate_textField.setBounds(493, 212, 191, 19);
+		profile.add(StartDate_textField);
+		
+		JLabel Additions = new JLabel("ADDITIONS");
+		Additions.setFont(new Font("Bahnschrift", Font.BOLD, 17));
+		Additions.setBounds(494, 274, 108, 28);
+		profile.add(Additions);
+		
+		Additions_textField = new JTextField();
+		Additions_textField.setColumns(10);
+		Additions_textField.setBounds(494, 319, 191, 19);
+		profile.add(Additions_textField);
+		
+		JPanel champsPlayer = new JPanel();
+		tabbedPane.addTab("CHAMPS", null, champsPlayer, null);
+		
+		JPanel champAdmin = new JPanel();
+		tabbedPane.addTab("CHAMPS", null, champAdmin, null);
+		
+		JPanel game = new JPanel();
+		tabbedPane.addTab("GAME", null, game, null);
+		
+		JPanel stadistics = new JPanel();
+		tabbedPane.addTab("STADISTICS", null, stadistics, null);
+		
+		JPanel management = new JPanel();
+		tabbedPane.addTab("MANAGEMENT", null, management, null);
+	}
+}
